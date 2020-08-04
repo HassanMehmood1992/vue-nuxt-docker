8456ebf673232029dafbf02480ff464551558fea
Hassan Mehmood <hassan.mehmood@click.aero>
2020-08-04 11:56:50 +0400

    project commit

---
+++ nuxt.config.js
@@ -0,0 +1,110 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'spa',
+  env: {
+    NUXT_ENV_ENVIRONMENT: process.env.NUXT_ENV_ENVIRONMENT,
+    NUXT_ENV_API_URL: process.env.NUXT_ENV_API_URL,
+    NUXT_ENV_ROUTER_BASE: process.env.NUXT_ENV_ROUTER_BASE
+  },
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+    "~/plugins/data",
+    "~/plugins/preload",
+    "~/plugins/axios"
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+ router: {
+  base: process.env.NUXT_ENV_ROUTER_BASE // It should contain the pre and post slashes
+},
+  buildModules: [
+    '@nuxt/typescript-build',
+    '@nuxtjs/vuetify',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: '#1F305E',
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        },
+        light: {
+          primary: '#1F305E',
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          accent: "#d68e25",
+          gred: "#EA4335",
+          ggreen: "#34A853",
+          ggrey: "#5C5C5C",
+          secondary: "#332334",
+          info: "#332334",
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  build: {
+  }
+}
